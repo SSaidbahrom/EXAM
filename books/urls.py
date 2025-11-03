518d843804a1812c887adf6806fa03307b17eea6
Cayomi <STCayom@gmail.com>
2025-11-03 20:11:36 +0500

    first commit

--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('<int:pk>/', views.book_detail, name='book_detail'),
+    path('', views.book_list, name='books_list'),
+    path('<int:pk>/', views.book_detail, name='books_detail'),
 ]
 
 
